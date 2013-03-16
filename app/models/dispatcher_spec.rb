2f29cf94a223ea43fe544542435750cf201773f8
Jordan Raine <jnraine@gmail.com>
2013-03-15 18:21:31 -0700

    Make dispatcher spec pass
    Updated with newest API

--- app/models/dispatcher_spec.rb
+++ app/models/dispatcher_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
 
 describe Dispatcher do
+  let(:twilio_params) do
+    {
+      "To" => "+15551234567",
+      "DialCallStatus" => "completed",
+      "CallSid" => "unique sid",
+      "From" => "+15557654321"
+    }
+  end
+
+  let(:dispatcher) { Dispatcher.new(twilio_params) }
+
   describe ".unavailable_number" do
     it "tells caller the number is not available" do
-      Dispatcher.unavailable_number({:to => "+15551234567"}).should == "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Say>+15551234567 is not available at this time. Sorry for any inconvenience</Say></Response>"
+      dispatcher.unavailable_number.should == "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Say>+15551234567 is not available at this time. Sorry for any inconvenience</Say></Response>"
     end
   end
 end
